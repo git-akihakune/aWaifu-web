e25c1d547e1642aa86b850341e50d882b374047d
git-akihakune <akihakune@protonmail.ch>
2021-11-17 16:58:19 +0700

    Load species name from config

--- aWaifu/web.py
+++ aWaifu/web.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import os
 from utils import security
 
-# Run a local server
 app = Flask(__name__,
             static_url_path='',
             static_folder='static',
             template_folder='templates')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/settings')
 def settings():
     return "This should be a dashboard, that let users configure some parameters on. It'll soon be developed."
+
 
 @app.route('/profile')
 def profile():
@@ -22,6 +24,7 @@ def profile():
     if os.environ["FLASK_ENV"] == "production" and security.checkPreviousSite(request.referrer):
         return redirect(url_for('index'))
     return render_template('profile.html')
+
 
 # A small Easter Egg
 @app.route('/admin')
--- awaifu.py
+++ awaifu.py
@@ -15,6 +15,11 @@ TURN_ON = ['True', 'true', '1', 'yes', 'Yes', 'YES', 'y', 'Y', 't', 'T', 'on', '
 if not config['DEFAULT']['dependency_installed'] in TURN_ON:
     if input('Dependencies not installed. Install now? ') in TURN_ON:
         os.system('pip install -r requirements.txt')
+        
+        # Update config file
+        config['DEFAULT']['dependency_installed'] = 'true'
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
 
 
 # Check for debug mode, then run server
