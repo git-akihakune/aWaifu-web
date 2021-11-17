96e3efef55b4d3f447747499420ab44acd9d598b
git-akihakune <akihakune@protonmail.ch>
2021-11-17 16:33:32 +0700

    Add load config function

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-import os
+#!/usr/bin/env python3
 
-def flaskEnvSetup():
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+TURN_ON = ['True', 'true', '1', 'yes', 'Yes', 'YES', 'y', 'Y', 't', 'T', 'on', 'On', 'ON']
+
+if config['DEFAULT']['debug'] in TURN_ON:
     os.environ["FLASK_APP"] = "aWaifu/web"
-    os.environ["FLASK_ENV"] = "production"
+    os.environ["FLASK_ENV"] = "development"
+    print("Running in debug mode")
     os.system("flask run")
+else:
+    print("Production mode detected.")
