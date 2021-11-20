ec94b769c032d32f7d59e961c062f745d2a93853
git-akihakune <akihakune@protonmail.ch>
2021-11-20 10:27:16 +0700

    Add more entities to config file

---
+++ aWaifu/utils/config.py
@@ -0,0 +1,11 @@
+import configparser
+import os
+
+configfile = 'config.ini'
+ROOT_DIR = os.path.dirname(os.path.abspath(configfile))
+
+config = configparser.ConfigParser()
+config.read(os.path.join(ROOT_DIR, configfile))
+
+RACE_NAMES = config['RACES']['races_name'].split('\n')
+domainName = config['WEB']['domain_name']
--- awaifu.py
+++ awaifu.py
@@ -3,29 +3,32 @@
 import os
 import configparser
 
-
+configfile = 'config.ini'
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(configfile)
 
 
 TURN_ON = ['True', 'true', '1', 'yes', 'Yes', 'YES', 'y', 'Y', 't', 'T', 'on', 'On', 'ON']
 
+debugMode = config['DEFAULT']['debug']
+dependencyInstalled = config['DEFAULT']['dependency_installed']
+
 
 # Check if dependencies are installed
-if not config['DEFAULT']['dependency_installed'] in TURN_ON:
+if dependencyInstalled not in TURN_ON and debugMode not in TURN_ON:
     if input('Dependencies not installed. Install now? ') in TURN_ON:
         os.system('pip install -r requirements.txt')
         
         # Update config file
         config['DEFAULT']['dependency_installed'] = 'true'
-        with open('config.ini', 'w') as configfile:
+        with open(configfile, 'w') as configfile:
             config.write(configfile)
 
 
 # Check for debug mode, then run server
-if config['DEFAULT']['debug'] in TURN_ON:
+if debugMode in TURN_ON:
     os.environ["FLASK_APP"] = "aWaifu/web"
     os.environ["FLASK_ENV"] = "development"
     os.system("flask run")
 else:
-    print("Production mode detected.")
+    print("Production mode detected. Please set up WSGI.")
