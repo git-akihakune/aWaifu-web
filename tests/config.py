26de78c5ce8d3e4c1097251a4a2c695db13ec2a6
git-akihakune <akihakune@protonmail.ch>
2021-11-29 17:39:29 +0700

    Add test

---
+++ tests/config.py
@@ -0,0 +1,10 @@
+import configparser
+import os
+
+configfile = 'config.ini'
+ROOT_DIR = os.path.dirname(os.path.abspath(configfile))
+
+config = configparser.ConfigParser()
+config.read(os.path.join(ROOT_DIR, configfile))
+
+verbose = config.getboolean('DEFAULT', 'verbose')
